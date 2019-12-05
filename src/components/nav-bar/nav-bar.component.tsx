40cf756c13d79e616ec7e7568957a1bd6108ffbe
MalindaK <kumarasinghe@axinom.com>
2019-12-05 12:46:03 +0530

    Added Reacr routing capabilities, Removed unused code

---
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+
+const NavBarComponent: React.FC = () => {
+    return (
+    <div>
+        Navigation Bar
+    </div>);
+}
+
+export default NavBarComponent;
